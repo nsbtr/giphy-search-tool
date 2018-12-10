9b91d1b38aba71d0e20cdfe17e9082cc8c6ad0aa
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-12-09 19:36:44 -0500

    base gif search

--- src/App.js
+++ src/App.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    const apiKey = '1RKXhyDiZ7ei9vWUKDXsxQc6yov96NSl';
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=dogs&limit=30`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(results => {
+        let items = results.data.map(item => ({
+          id: item.id,
+          url: item.url,
+          images: item.images,
+          title: item.title,
+        }));
+
+        this.setState({ items });
+      });
+  }
+
   render() {
-    return <div className="App">Awesome giphy search app :D</div>;
+    return (
+      <div className="App">
+        Awesome giphy search app :D
+        {this.state.items.map(item => (
+          <img
+            src={item.images.fixed_height.url}
+            key={item.id}
+            alt={item.title}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
