cd4ff2ba7dc264dfdccadb42da64211ea1a4b35e
Nicole Sabater <nicole.sabater@climaxmedia.com>
2018-12-11 01:08:03 -0500

    media queries

---
+++ src/utils/media.js
@@ -0,0 +1,38 @@
+import { css } from 'styled-components';
+
+export const devices = {
+  mobile: 'mobile',
+  tablet: 'tablet',
+  desktop: 'desktop',
+};
+
+/*
+ * styled.div`
+ *   overflow: hidden;
+ *   ${media.mobile`
+ *    width: 100%
+ *   `}*
+ */
+
+const media = {
+  [devices.mobile]: (...args) =>
+    css`
+      @media screen and (max-width: 767px) {
+        ${css(...args)};
+      }
+    `,
+  [devices.tablet]: (...args) =>
+    css`
+      @media screen and (min-width: 768px) and (max-width: 1023px) {
+        ${css(...args)};
+      }
+    `,
+  [devices.desktop]: (...args) =>
+    css`
+      @media screen and (min-width: 1024px) {
+        ${css(...args)};
+      }
+    `,
+};
+
+export default media;
